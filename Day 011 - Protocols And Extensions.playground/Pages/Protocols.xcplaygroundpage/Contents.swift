60bc3177edf64350901e4593c131bc4732f170de
Eduardo Righes <eduardo.righes@gmail.com>
2021-05-03 21:40:47 +0100

    100 Days Of Swift Day 11 - Additional Examples

--- Day 011 - Protocols And Extensions.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ Day 011 - Protocols And Extensions.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -15,4 +15,44 @@ func displayId(thing: Identifiable) {
 let user = User(id: "my_id")
 displayId(thing: user)
 
+// MARK: Additional Examples
+
+protocol Purchasable {
+    var name: String { get set }
+}
+
+struct Book: Purchasable {
+    var name: String
+    var author: String
+}
+
+struct Movie: Purchasable {
+    var name: String
+    var actors: [String]
+}
+
+struct Car: Purchasable {
+    var name: String
+    var manufacturer: String
+}
+
+struct Coffee: Purchasable {
+    var name: String
+    var strength: Int
+}
+
+func buy(_ item: Purchasable) {
+    print("I am buying \(item.name)")
+}
+
+let book = Book(name: "Ready Player One", author: "Ernest Cline")
+let movie = Movie(name: "The Matrix", actors: ["Keanu Reeves", "Laurence Fishburne"])
+let car = Car(name: "HR-V", manufacturer: "Honda")
+let coffee = Coffee(name: "Lavazza", strength: 5)
+
+buy(book)
+buy(movie)
+buy(car)
+buy(coffee)
+
 //: [Next](@next)
