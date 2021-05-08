67d4e3a005dfb5a1102e62470e20023aa27078cd
Eduardo Righes <eduardo.righes@gmail.com>
2021-05-08 16:02:57 +0100

    100 Days Of Swift Day 16 - Project1, part one

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Eduardo Maestri Righes on 08/05/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // An iOS app always has a resourcePath,
+        // so it is OK to force unwrap the value
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+
+}
+
