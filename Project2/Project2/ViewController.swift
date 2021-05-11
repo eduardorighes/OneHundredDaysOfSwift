1390eed1890a0df354b52df2980ecee9144ae29c
Eduardo Righes <eduardo.righes@gmail.com>
2021-05-11 22:57:34 +0100

    100 Days Of Swift Day 19 - Project2, part one

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Eduardo Maestri Righes on 11/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += [
+            "estonia",
+            "france",
+            "germany",
+            "ireland",
+            "italy",
+            "monaco",
+            "nigeria",
+            "poland",
+            "russia",
+            "spain",
+            "uk",
+            "us"
+        ]
+        
+        setupButtons()
+        askQuestion()
+    }
+    
+    func setupButtons() {
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+
+
+}
+
