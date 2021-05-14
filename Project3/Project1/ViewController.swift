ac66ffe23c331d938090efeef10fa8153d151d2f
Eduardo Righes <eduardo.righes@gmail.com>
2021-05-15 00:37:35 +0100

    100 Days Of Swift Day 22 - Project3
    Note: this is based on Project1; keeping things apart in order to follow the tutorials.

---
+++ Project3/Project1/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Eduardo Maestri Righes on 08/05/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // An iOS app always has a resourcePath,
+        // so it is OK to force unwrap the value
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        pictures.sort()
+        
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.imageIndex = indexPath.row
+            vc.imageCount = pictures.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
