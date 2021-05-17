963f785cddeeead5b95f6d9daa407fa47b93454e
Eduardo Righes <eduardo.righes@gmail.com>
2021-05-17 21:12:08 +0100

    100 Days Of Swift Day 25 - Project4, part two
    * Refactoring
    * UIProgressView
    * WebView decide policy for navigation action

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -11,6 +11,11 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    let websites = [
+        "hackingwithswift.com",
+        "apple.com"
+    ]
     
     override func loadView() {
         webView = WKWebView()
@@ -23,23 +28,37 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progress = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progress, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
 
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
     }
     
     func openPage(action: UIAlertAction) {
-        guard let pageAddress = action.title else { return }
-        guard let url = URL(string: "https://\(pageAddress)") else { return }
+        guard let website = action.title else { return }
+        guard let url = URL(string: "https://" + website) else { return }
         webView.load(URLRequest(url: url))
     }
     
@@ -47,6 +66,38 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        if let urlForAction = url {
+            print(urlForAction.absoluteString)
+            // Hacky... first webView load is 'about:blank', which
+            // triggers an alert. I don't want to show the alert in
+            // that case.
+            if (urlForAction.absoluteString != "about:blank") {
+                let warning = UIAlertController(title: "Website Not Allowed", message: "This website is not in the allow list.", preferredStyle: .alert)
+                warning.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+                present(warning, animated: true)
+            }
+        }
+        decisionHandler(.cancel)
+    }
+    
+    // MARK: - KVO
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
 }
 
