dd64dc1978b69c70916167cab017d7faa5f8493e
Eduardo Righes <eduardo.righes@gmail.com>
2021-05-19 22:51:28 +0100

    100 Days Of Swift Day 27 - Project5, part one
    * Read list of words from file
    * Setup basic UI
    * Text field in UIAlertAction
    * Capture lists (strong, weak, unowned)

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Eduardo Maestri Righes on 19/05/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
+        if let usedWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: usedWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            // This shouldn't happen, just an indicator something is wrong
+            allWords = [ "silkworm" ]
+        }
+        
+        startGame()
+    }
+
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        print(answer)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+}
+
