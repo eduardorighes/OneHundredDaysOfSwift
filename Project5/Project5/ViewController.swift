e68110bfa13f8280a33f47cc9d36bdf2a4a86ce9
Eduardo Righes <eduardo.righes@gmail.com>
2021-05-21 20:12:55 +0100

    100 Days Of Swift Day 29 - Project5, part three
    * Refactor
    * Insert word lowercase
    * Start game button

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(startGame))
         
         if let usedWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: usedWordsURL) {
@@ -31,7 +32,7 @@ class ViewController: UITableViewController {
         startGame()
     }
 
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -48,39 +49,44 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
-    func submit(_ answer: String) {
-        let lowerAnswer = answer.lowercased()
-        
-        let errorTitle: String
-        let errorMessage: String
-        
-        if isPossible(word: lowerAnswer) {
-            if isOriginal(word: lowerAnswer) {
-                if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
-                    // we have our usedWords[0] in place so once we
-                    // add the table row, it will fetch that
-                    let indexPath = IndexPath(row: 0, section: 0)
-                    tableView.insertRows(at: [indexPath], with: .automatic)
-                    
-                    return
-                } else {
-                    errorTitle = "Word not recognised"
-                    errorMessage = "You can't just make them up, you know!"
-                }
-            } else {
-                errorTitle = "Word already used"
-                errorMessage = "Be more original!"
-            }
-        } else {
-            guard let title = title?.lowercased() else { return }
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title)"
-        }
-        
+    func showErrorMessage(title errorTitle: String, message errorMessage: String) {
         let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        let startWord = title!
+        let lowerAnswer = answer.lowercased()
+        
+        if lowerAnswer == startWord {
+            showErrorMessage(title: "Word is start word", message: "You can't use the start word!")
+        } else {
+            if isPossible(word: lowerAnswer) {
+                if isOriginal(word: lowerAnswer) {
+                    if isReal(word: lowerAnswer) {
+                        //
+                        // About the debug challenge...
+                        // Insert lowercase or do isOriginal to be case-insensitive
+                        //
+                        usedWords.insert(lowerAnswer, at: 0)
+                        // we have our usedWords[0] in place so once we
+                        // add the table row, it will fetch that
+                        let indexPath = IndexPath(row: 0, section: 0)
+                        tableView.insertRows(at: [indexPath], with: .automatic)
+                        
+                        return
+                    } else {
+                        showErrorMessage(title: "Word not recognised", message: "You can't make them up, you know!")
+                    }
+                } else {
+                    showErrorMessage(title: "Word already used", message: "Be more original!")
+                }
+            } else {
+                guard let title = title?.lowercased() else { return }
+                showErrorMessage(title: "Word not possible", message: "You can't spell that word from \(title)")
+            }
+        }
     }
     
     func isPossible(word: String) -> Bool {
@@ -97,10 +103,23 @@ class ViewController: UITableViewController {
     }
     
     func isOriginal(word: String) -> Bool {
+        //
+        // If you want to make the check case insensitive...
+        //
+        
+        //return !usedWords.contains { $0.lowercased() == word }
+        
+        //return !usedWords.contains { item in
+        //    item.lowercased() == word
+        //}
+        
         return !usedWords.contains(word)
     }
     
     func isReal(word: String) -> Bool {
+        if word.count < 3 {
+            return false
+        }
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
