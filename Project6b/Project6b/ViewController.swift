8ff2166c1c6a9283199600b90634e7312a6e5520
Eduardo Righes <eduardo.righes@gmail.com>
2021-05-23 20:31:37 +0100

    Day 31 - Project 6b, part two
    * More visual formatting language
    * Ancors

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -50,12 +50,38 @@ class ViewController: UIViewController {
             "label5": label5
         ]
         
-        for (labelName, labelView) in viewDictionary {
+        for labelView in viewDictionary.values {
             view.addSubview(labelView)
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(labelName)]|", options: [], metrics: nil, views: viewDictionary))
         }
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewDictionary))
+//        for labelName in viewDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(labelName)]|", options: [], metrics: nil, views: viewDictionary))
+//        }
+//
+//        let metrics = [ "labelHeight": 88 ]
+//
+//        // @999 means priority 999, which is optional but high in the list.
+//        // @1000 (default) must be met (required).
+//        // Labels 2 to 5 must have the same height and priority of label 1.
+//        // The last label must be 10 points or more from the bottom of the screen.
+//
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewDictionary))
+        
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            if let previous = previous {
+                // we have a previous label - create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            // set the previous label to be the current one, for the next loop iteration
+            previous = label
+        }
     }
 }
 
