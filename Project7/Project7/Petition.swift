81e92395ef52ee687ed4b0a4483b10a67e229c38
Eduardo Righes <eduardo.righes@gmail.com>
2021-05-25 21:57:47 +0100

    Day 33 - Project7, part one
    * Create project
    * Setup table view embedded in navigation controller, which is embedded in a tab bar controller.
    * Parse JSON data
    * Load table view with parsed data

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Eduardo Maestri Righes on 25/05/2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Eduardo Maestri Righes on 25/05/2021.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
