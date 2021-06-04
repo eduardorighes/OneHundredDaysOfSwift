aabef0446964acac51ddc0c15d4a05865d073cb0
Eduardo Righes <eduardo.righes@gmail.com>
2021-06-04 23:26:26 +0100

    Day 43 - Project10, part two
    * UIImagePickerController
    * UIImagePickerControllerDelegate methods (didFinishPickingMediaWithInfo)
    * UICollectionViewController (didSelectItemAt)
    * FileManager.default.urls(for:in:)

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Eduardo Maestri Righes on 04/06/2021.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
