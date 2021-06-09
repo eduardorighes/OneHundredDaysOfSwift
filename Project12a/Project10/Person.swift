3d3be6ad668e7e9ce610b3cea28cfbb16f0b60ad
Eduardo Righes <eduardo.righes@gmail.com>
2021-06-09 22:17:43 +0100

    Day 48 - Project12, part one
    Introduction to
    
    * NSCoding
    * NSCoder
    * NSKeyedArchiver
    * NSKeyedUnarchiver

---
+++ Project12a/Project10/Person.swift
@@ -0,0 +1,28 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Eduardo Maestri Righes on 04/06/2021.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init?(coder: NSCoder) {
+        self.name = coder.decodeObject(forKey: "name") as? String ?? ""
+        self.image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+}
---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Eduardo Maestri Righes on 03/06/2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+}
