03e26311d03f7bcb35d17bad759baccc3d4958e2
Eduardo Righes <eduardo.righes@gmail.com>
2021-06-10 21:10:02 +0100

    Day 49 - Project12 Challenge 1
    * Save number of times an image is viewed

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    // I know I could have just a dictionary for everything
+    // but I want to leave things separate in case I still
+    // need to change this project
     var pictures = [String]()
+    var imageViewCounter = [String: Int]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +35,8 @@ class ViewController: UITableViewController {
             }
             self?.pictures.sort()
             
+            self?.load()
+            
             DispatchQueue.main.async { [weak self] in
                 self?.tableView.reloadData()
             }
@@ -46,13 +52,21 @@ class ViewController: UITableViewController {
         present(vc, animated: true)
     }
     
+    func imageViewCount(forPath indexPath: IndexPath) -> Int {
+        return imageViewCounter[pictures[indexPath.row]] ?? 0
+    }
+    
+    func imageViewCount(forImage imageName: String) -> Int {
+        return imageViewCounter[imageName] ?? 0
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = "\(pictures[indexPath.row]) (\(imageViewCount(forPath: indexPath)) views)"
         return cell
     }
 
@@ -62,6 +76,32 @@ class ViewController: UITableViewController {
             vc.imageIndex = indexPath.row
             vc.imageCount = pictures.count
             navigationController?.pushViewController(vc, animated: true)
+            let currentViewCounter = imageViewCounter[pictures[indexPath.row]] ?? 0
+            imageViewCounter[pictures[indexPath.row]] = currentViewCounter + 1
+            save()
+            tableView.reloadData()
+        }
+    }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(imageViewCounter) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "imageViewCounter")
+        } else {
+            print("Failed to encode data.")
+        }
+    }
+    
+    func load() {
+        let defaults = UserDefaults.standard
+        if let savedData = defaults.object(forKey: "imageViewCounter") as? Data {
+            do {
+                let jsonDecoder = JSONDecoder()
+                imageViewCounter = try jsonDecoder.decode([String: Int].self, from: savedData)
+            } catch {
+                print("Failed to load data.")
+            }
         }
     }
 }
