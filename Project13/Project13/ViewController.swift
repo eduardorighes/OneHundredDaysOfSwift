a313da557e6664d3f344dad082650fd27819de5f
Eduardo Righes <eduardo.righes@gmail.com>
2021-06-15 22:56:16 +0100

    Day 52 - Project13, part one
    Instafilter
    
    * Designed basic UI

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Eduardo Maestri Righes on 15/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true)
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+}
+
