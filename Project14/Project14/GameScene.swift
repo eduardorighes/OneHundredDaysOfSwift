9db5b87bc340de79faad85d0ef07a6a73baaf367
Eduardo Righes <eduardo.righes@gmail.com>
2021-06-17 19:08:09 +0100

    Day 55 - Project14, part one
    * Initial project setup (empty)
    * Graphics
    * AppIcon

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Eduardo Maestri Righes on 17/06/2021.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
