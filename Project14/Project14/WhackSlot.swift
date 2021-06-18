fd6800af89920a71ec9cabe57ec8338744930e8b
Eduardo Righes <eduardo.righes@gmail.com>
2021-06-19 00:29:48 +0100

    Day 56 - Project14 Challenge 3
    * Add smoke particle effect when hitting the penguin.

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -67,6 +67,12 @@ class WhackSlot: SKNode {
     func hit() {
         isHit = true
         
+        if let smoke = SKEmitterNode(fileNamed: "Smoke") {
+            smoke.position = charNode.position
+            smoke.zPosition = 1
+            addChild(smoke)
+        }
+        
         let delay = SKAction.wait(forDuration: 0.25)
         let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
         let notVisible = SKAction.run{ [weak self] in self?.isVisible = false }
